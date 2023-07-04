5af3eb1c3a5ecf6f2606045f4447986e8ac242aa
Sardor <131400267+DeveloperSardor@users.noreply.github.com>
2023-07-04 17:14:37 +0500

    Update server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 const PORT = config.get("port") || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors('*'));
 app.use("/api", indexRouter);
 
 app.listen(PORT, () => console.log(`Server is running ${PORT}...`));
